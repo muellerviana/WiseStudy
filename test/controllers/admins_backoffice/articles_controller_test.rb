fb29b3d3c3a62e1abcf13b8f3d09b58e0e969131
muellerviana <muellerviana@gmail.com>
2024-01-02 18:49:24 -0300

    cancell automated generation of tests

--- test/controllers/admins_backoffice/articles_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class AdminsBackoffice::ArticlesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get admins_backoffice_articles_index_url
-    assert_response :success
-  end
-end
