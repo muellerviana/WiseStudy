3a467640a7bd36a65a9257843eb20762ec2dc3c4
muellerviana <muellerviana@gmail.com>
2024-01-09 23:37:34 -0300

    create table for articles

--- app/controllers/admins_backoffice/articles_controller.rb
+++ app/controllers/admins_backoffice/articles_controller.rb
@@ -2,4 +2,8 @@ class AdminsBackoffice::ArticlesController < AdminsBackofficeController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
