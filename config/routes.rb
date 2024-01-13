054d1f14649777386acf63be6cc94b19d4145a0e
muellerviana <muellerviana@gmail.com>
2024-01-13 13:46:48 -0300

    fix site home page

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   namespace :site do
     get 'home/index'
+
+    resources :articles
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
