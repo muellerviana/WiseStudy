deb0a44e4612c1f3afec1fae2fc2ba448e8118ff
muellerviana <muellerviana@gmail.com>
2024-01-30 22:14:35 -0300

    remove registrable from devise

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,8 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :registerable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable,
+         :rememberable,
+         :validatable
 end
