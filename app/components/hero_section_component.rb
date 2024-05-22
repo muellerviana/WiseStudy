7e083b9c2e203643e93bb448bad89bca3a79ccdd
muellerviana <muellerviana@gmail.com>
2024-05-22 19:39:12 -0300

    fix image component and file

--- app/components/hero_section_component.rb
+++ app/components/hero_section_component.rb
@@ -1,9 +1,10 @@
 # frozen_string_literal: true
 
 class HeroSectionComponent < ViewComponent::Base
- def initialize(folder_link:, title:, text:)
+ def initialize(folder_link:, title:, text:, css_classes:)
     @folder_link = folder_link
     @title = title
     @text = text
+    @css_classes = css_classes
   end
 end
