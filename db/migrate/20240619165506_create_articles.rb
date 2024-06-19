d04d86f3892ebdfe5a6c97368f16c1f7601486c5
muellerviana <muellerviana@gmail.com>
2024-06-19 14:42:48 -0300

    create articles table

---
+++ db/migrate/20240619165506_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[7.1]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :description
+      t.text :body
+      t.string :image
+      t.string :url
+
+      t.timestamps
+    end
+  end
+end
