11e1e06cffdc920155f6d31b0df4b9b10d41077d
muellerviana <muellerviana@gmail.com>
2024-06-22 19:45:25 -0300

    create actions for articles controller

--- app/controllers/admins_dashboard/articles_controller.rb
+++ app/controllers/admins_dashboard/articles_controller.rb
@@ -2,4 +2,22 @@ class AdminsDashboard::ArticlesController < AdminsDashboardController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to admins_backoffice_articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 end
