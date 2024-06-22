68876aa6e47e5bfdfd967edb582ca926de3a1100
muellerviana <muellerviana@gmail.com>
2024-06-22 19:44:02 -0300

    limit actions for home controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   namespace :admins_dashboard do
     resources :articles
-    resources :home
+    resources :home, only: [:index]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
