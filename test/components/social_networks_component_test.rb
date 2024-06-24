03870dcf878e0cbeadffad1a0bd2d5d122a6baa4
muellerviana <muellerviana@gmail.com>
2024-06-24 17:57:01 -0300

    create social networks component

---
+++ test/components/social_networks_component_test.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class SocialNetworksComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(SocialNetworksComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
