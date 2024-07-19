792d16bc34b434841048f5d16337150827f3cb79
muellerviana <muellerviana@gmail.com>
2024-07-18 21:34:54 -0300

    add shared form for new and update actions

--- app/components/link_component.rb
+++ app/components/link_component.rb
@@ -2,9 +2,10 @@
 
 class LinkComponent < ViewComponent::Base
 
-  def initialize(text:, url:, method: :get)
+  def initialize(text:, url:, method: :get, confirm: nil)
     @url = url
     @text = text
     @method = method
+    @confirm = confirm
   end
 end
