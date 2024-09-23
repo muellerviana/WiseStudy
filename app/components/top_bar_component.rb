762d0638bd5835e9012f4ba424a7aef6d7d3fbb1
muellerviana <muellerviana@gmail.com>
2024-09-23 13:09:37 -0300

    render user actions in top_bar_component

--- app/components/top_bar_component.rb
+++ app/components/top_bar_component.rb
@@ -2,5 +2,6 @@
 class TopBarComponent < ViewComponent::Base
   renders_one :logotype, LogotypeComponent
   renders_one :search_bar, SearchBarComponent
-  renders_one :social_network, SocialNetworksComponent
+  renders_one :social_networks, SocialNetworksComponent
+  renders_one :user_actions, UserActionsComponent
 end
