3cd61ba828b0a3178b6eeeec0fb1df5a1998d0b2
Miqueias Viana <miqueiasmenddes@gmail.com>
2024-11-27 22:24:31 -0300

    Hide menu when click outside

--- app/javascript/controllers/button_menu_controller.js
+++ app/javascript/controllers/button_menu_controller.js
@@ -1,8 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-toggleDiv() {
-    const event = new CustomEvent("toggle:visibility", { bubbles: true });
-    window.dispatchEvent(event); // Dispatch the event globally
-}
-}
+    toggleDiv() {
+        setTimeout(() => {
+          const event = new CustomEvent("toggle:visibility", { bubbles: true });
+          window.dispatchEvent(event);
+        }, 0);
+      }
+    }      
