24cc9c41077a2ef3cf84c274db5aedd43619ee7e
muellerviana <muellerviana@gmail.com>
2024-12-03 17:29:31 -0300

    edit button functions

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class ButtonComponent < ViewComponent::Base
-  def initialize(text:)
+  def initialize(text:, link:)
     @text = text
+    @link = link
   end
 end
--- app/controllers/admins_dashboard/books_controller.rb
+++ app/controllers/admins_dashboard/books_controller.rb
@@ -6,4 +6,24 @@ class AdminsDashboard::BooksController < AdminsDashboardController
   def show
     @book = Book.includes(:authors).find(params[:id])
   end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      redirect_to admins_dashboard_books_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def book_params
+    params.require(:book).permit(:title, :publisher, :publication_year, :isbn, :summary, :genre, :language)
+  end
 end
