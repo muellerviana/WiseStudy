e081d1075b3e5f61a54bc3e095f062303809f8a7
muellerviana <muellerviana@gmail.com>
2024-12-03 13:27:33 -0300

    Created Books Show Page for Admins

--- app/controllers/admins_dashboard/books_controller.rb
+++ app/controllers/admins_dashboard/books_controller.rb
@@ -2,4 +2,8 @@ class AdminsDashboard::BooksController < AdminsDashboardController
   def index
     @books = Book.all
   end
+
+  def show
+    @book = Book.includes(:authors).find(params[:id])
+  end
 end
