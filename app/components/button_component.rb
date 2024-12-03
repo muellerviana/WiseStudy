829d2260acf5f0e81de6a25e7477f7abc8135767
muellerviana <muellerviana@gmail.com>
2024-12-03 18:19:15 -0300

    update button_component for dynamic params

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class ButtonComponent < ViewComponent::Base
-  def initialize(text:, link:)
+  def initialize(text:, link:, method: :get, confirm_message: nil)
     @text = text
     @link = link
+    @turbo_method = method
+    @confirm_message = confirm_message
   end
 end
