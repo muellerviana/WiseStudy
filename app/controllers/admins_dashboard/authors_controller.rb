d8eb1205eed752321885b89cb742ab2cef945237
muellerviana <muellerviana@gmail.com>
2024-12-03 22:49:53 -0300

    Author field in admins_dashboard

---
+++ app/controllers/admins_dashboard/authors_controller.rb
@@ -0,0 +1,51 @@
+class AdminsDashboard::AuthorsController < AdminsDashboardController
+  def index
+    @authors = Author.all
+  end
+
+  def show
+    @author = Author.includes(:books).find(params[:id])
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+
+    if @author.save
+      redirect_to admins_dashboard_authors_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @author = Author.find(params[:id])
+  end
+
+  def update
+    @author = Author.find(params[:id])
+
+    if @author.update(author_params)
+      redirect_to admins_dashboard_authors_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @author = Author.find(params[:id])
+
+    @author.destroy
+    redirect_to admins_dashboard_authors_path, status: :see_other
+  end
+
+
+  private 
+
+  def author_params
+    params.require(:author).permit(:name, :last_name, :bio, :birth_date, :death_date, :nationality, :book_avatar, :website)
+  end
+end
---
+++ app/helpers/admins_dashboard/authors_helper.rb
@@ -0,0 +1,2 @@
+module AdminsDashboard::AuthorsHelper
+end
