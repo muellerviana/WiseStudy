4885a5b8ed158084af4b55bafb49c01d8f9147ae
muellerviana <muellerviana@gmail.com>
2024-12-05 18:22:58 -0300

    fix config js to accept dinamics components

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,10 +3,23 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './public/*.html',
+    './app/assets/stylesheets/application.tailwind.css',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}',
-    './app/components/**/*.{erb,haml,html,slim}'
+    './app/inputs/**/*.rb',
+    './app/views/**/*.{erb,haml,html,slim,rb}',
+    './app/view_components/**/*.{erb,html,rb}',
+    './app/view_components/previews/**/*.{erb,html,rb}',
+    './app/components/**/*.{erb,haml,html,slim}',
+    './app/assets/stylesheets/**/*.css',
+    './spec/view_components/previews/**/*.{erb,html,rb',
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './src/pages/**/*.{html,js}',
+    './src/components/**/*.{html,js}',
+    './app/components/**/*.{erb,rb}', // Custom components
+    './src/layouts/**/*.{html,js}',
+    './src/index.html',
   ],
   theme: {
     extend: {
