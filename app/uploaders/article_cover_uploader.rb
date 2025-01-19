82ad9e6bcb73726ac78c5c8e8ac7640d71b33fbb
muellerviana <muellerviana@gmail.com>
2025-01-18 21:34:09 -0300

    Add feature for resize image size when upload

--- app/uploaders/article_cover_uploader.rb
+++ app/uploaders/article_cover_uploader.rb
@@ -1,7 +1,7 @@
 class ArticleCoverUploader < CarrierWave::Uploader::Base
-  # Include RMagick or MiniMagick support:
+  #Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
@@ -23,6 +23,7 @@ class ArticleCoverUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
+  process resize_to_limit: [400, 300] # Resizes without exceeding 800x800
   #
   # def scale(width, height)
   #   # do something
@@ -36,7 +37,7 @@ class ArticleCoverUploader < CarrierWave::Uploader::Base
   # Add an allowlist of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_allowlist
-    %w(jpg jpeg png)
+    %w(jpg jpeg png webp)
   end
 
   # Override the filename of the uploaded files:
